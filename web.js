4ca2c1d571c6389ca588e67ad9e6d2448cc4141b
ALenfant <antonin.lenfant@gmail.com>
2013-07-11 05:45:09 +0200

    Display content of index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	filebuffer = fs.readFileSync('index.html')
+	filestring = filebuffer.toString()
+	response.send(filestring);
 });
 
 var port = process.env.PORT || 5000;
